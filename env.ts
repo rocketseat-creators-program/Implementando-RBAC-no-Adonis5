e0d2b5fef7bc54dced0220c0579199aa2b38562a
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2021-07-24 17:51:20 -0300

    chore: lucid (orm) setup

--- env.ts
+++ env.ts
@@ -20,4 +20,6 @@ export default Env.rules({
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+
+  DB_CONNECTION: Env.schema.string(),
 })
